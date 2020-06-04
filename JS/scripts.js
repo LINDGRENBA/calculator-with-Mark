a1d739cb9ddf2296fe0236f971bd108ff50eb1eb
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-04 15:41:20 -0700

    Update scripts.js with refactored code.
    Co-authored by: Mark Tiapkov <mark.tiapkov@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,4 @@
+//BUSINESS LOGIC
 const add = function(number1, number2) {
   return number1 + number2;
 };
@@ -12,36 +13,16 @@ const divide = function(number1, number2) {
   return number1 / number2;
 };
 
+
+//USER INTERFACE LOGIC
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operation = $("input:radio[name=operator]:checked").val();
+
     const result = add(number1, number2);
-    $("#outputadd").text(result);
-  });
-
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#subtract1").val());
-    const number2 = parseInt($("#subtract2").val());
-    const result = subtract(number1, number2);
-    $("#outputsubtract").text(result);
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#multiply1").val());
-    const number2 = parseInt($("#multiply2").val());
-    const result = multiply(number1, number2);
-    $("#outputmultiply").text(result);
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#divide1").val());
-    const number2 = parseInt($("#divide2").val());
-    const result = divide(number1, number2);
-    $("#outputdivide").text(result);
+    $("#output").text(result);
   });
 });
